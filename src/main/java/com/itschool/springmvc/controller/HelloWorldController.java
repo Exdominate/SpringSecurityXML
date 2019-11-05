83aa4a7090a8db19e1e616e14a42ce64b03b5710
Mikhail <>
2019-11-05 15:55:07 +0300

    Not work, but will be fixed later

---
+++ src/main/java/com/itschool/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.itschool.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = {"/", "/welcome**"}, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is the welcome page!");
+        model.setViewName("welcome");
+        return model;
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Hello World");
+        model.addObject("message", "This is the protected welcome page!");
+        model.setViewName("admin");
+        return model;
+    }
+
+}
